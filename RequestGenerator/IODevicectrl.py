abdcbf17bdc788ccf88eda067040694d4ecf4d49
wiseo <swi7982@gmail.com>
2021-06-14 15:10:13 +0900

    add OutputDtoH.py

--- RequestGenerator/IODevicectrl.py
+++ RequestGenerator/IODevicectrl.py
@@ -8,7 +8,7 @@ import math
 import random
 
 # Push Input Request to Device &
-# Pull Result from Device 
+# Pull Results from Device 
 
 
 # Push Input Request to Android Device
@@ -24,6 +24,50 @@ def pushInputsToDevice(localPath, remotePath):
 	for i in range(0, len(onlyfiles)):
 		cmd = "adb push " + localPath + onlyfiles[i] + " " + remotePath + "I/" 
 		os.system(cmd)
+
+
+# Pull Results from Device 
+
+def pull_subset(Req_name, Result_path, algo):
+	if os.path.isdir(Result_path) == False:
+		os.system("mkdir " + Result_path)
+
+	algo_path = Result_path + "/" + Req_name + algo + "_O"
+
+	os.system("mkdir " + algo_path)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+algo+"_O/ "+algo_path)
+
+def pull(Req_name, Result_path):
+	
+	if os.path.isdir(Result_path) == False:
+		os.system("mkdir " + Result_path)
+		
+	gpu = Result_path + "/" + Req_name + "gpu_O"
+	dsp = Result_path + "/" + Req_name + "dsp_O"
+	lb = Result_path + "/" + Req_name + "lb_O"
+	st = Result_path + "/" + Req_name + "st_O"
+	my = Result_path + "/" + Req_name + "my_O"
+	slo = Result_path + "/" + Req_name + "slo_O"
+	slo_div = Result_path + "/" + Req_name + "slo_div_O"
+		
+	os.system("mkdir " + gpu)
+	os.system("mkdir " + dsp)
+	os.system("mkdir " + lb)
+	os.system("mkdir " + st)
+	os.system("mkdir " + my)
+	os.system("mkdir " + slo)
+	os.system("mkdir " + slo_div)
+	
+	
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"gpu_O/ " +gpu)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"dsp_O/ " +dsp)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"lb_O/ " +lb)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"st_O/ " +st)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"my_O/ " +my)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"slo_O/ " +slo)
+	os.system("adb pull /data/local/tmp/request_file/"+Req_name+"slo_div_O/ " +slo_div)
+
+
 
 
 if __name__== "__main__":
